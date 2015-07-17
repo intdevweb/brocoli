9e8ba8c0aa6b44ab63d72dca39877d10db688778
intdevweb <jp_informatique@hotmail.fr>
2015-07-17 10:45:39 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>google</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/style1.css">	
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<h1>liste deroulante</h1>
+	<div id="affiche">
+	<select name="list" action="">
+		<option value="0">faites un choix</option>
+		<option value="2">internet explorer</option>
+		<option value="3">google</option>
+		<option value="4">firefox</option>
+		<input type="button" value" reponse" id="btn" onclick="list (f)"/>
+	</form>
+</body>
+</html>
